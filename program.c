57bc40256a9117158bd1c78b57bc4783e331eebd
Douglas Chidester <tikiman92@gmail.com>
2015-08-25 22:08:28 -0700

    Added license.

--- program.c
+++ program.c
@@ -1,8 +1,28 @@
+/*
+	Example program to demonstrate pthreads.
+    Copyright (C) 2015  Doulas Chidester
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #include "program.h"
 
 int main(int argc, char* argv[])
 {
 	ThreadData td[NUM_THREADS];
+
+	puts(license);
 
 	createThreads(td);
 	
--- program.h
+++ program.h
@@ -1,3 +1,21 @@
+/*
+	Example program to demonstrate pthreads.
+    Copyright (C) 2015  Doulas Chidester
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
 #ifndef PROGRAM_H
 #define PROGRAM_H
 
@@ -18,6 +36,8 @@ typedef struct
 	pthread_t tid;
 	char message[MSG_SIZE];
 } ThreadData;
+
+char* license = "Copyright (C) 2015 Douglas Chidester. This program comes with ABSOLUTELY NO WARRANTY; This is free software, and you are welcome to redistribute it under certain conditions.";
 
 void errorHandle(const char* msg); // TODO use <stdarg.h> to make this function take variable # of args
 void* threadFunction(void* arg);
