c4845e1ebc78cac06dd1bf1e237e688e80814d72
Douglas Chidester <Douglas.Chidester@faradayfuture.com>
2015-08-25 17:13:05 -0700

    wrote simple example

--- program.c
+++ program.c
@@ -2,6 +2,32 @@
 
 int main(int argc, char* argv[])
 {
-	puts("just testing");	
+	ThreadData td[NUM_THREADS];
+	char tMsg = "Hello from thread ";
+	
+	// createThreads(...)
+	for(int i = 0; i < NUM_THREADS; ++i)
+	{
+		td[i].id = i+1;
+		strcpy(td[i].message, tMsg);
+		pthread_create(&td[i].tid, NULL, threadFunction, td);
+	}
+
+	puts("Main thread created all child threads..");
+
+	// joinThreads(...);
+	for(int j = 0; j < NUM_THREADS; ++j)
+		pthread_join(td[i].tid, NULL);
+	puts("Main thread joined all child threads. Now exiting");
+
 	return 0;
+}
+
+void* threadFunction(void* arg)
+{
+	ThreadData* data = (ThreadData*) arg;
+	
+	printf("[%d] %s %d.\n", data->tid, data->message, data->id);
+
+	pthread_exit(0);
 }
--- program.h
+++ program.h
@@ -2,5 +2,19 @@
 #define PROGRAM_H
 
 #include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+
+#define MSG_SIZE 20
+#define NUM_THREADS 4
+
+typedef struct
+{
+	uint8 id;
+	pthread_t tid;
+	char message[MSG_SIZE];
+} ThreadData;
+
+void threadFunction(void* data);
 
 #endif
